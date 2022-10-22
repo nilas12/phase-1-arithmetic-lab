6b77656d47c8be7249a7886c9889e99c0f41bf14
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-22 22:09:15 +0300

    undone

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
-// Write your code here
+function multiply(){
+    let num1 = 31
+    let num2 = 2
+  
+    return num1*num2 }
+    function random(max) {
+        return Math.floor(Math.random() * max);
+      }
+      console.log(Math.random());
+      function max(numArray) {
+        return Math.max.apply(null, numArray);
+      }
+      function mod(x) {
+        return math.mod(x, 2) != 0
+      }
+      //I DO NOT KNOW WHY NOT PASSING THE TEST??
